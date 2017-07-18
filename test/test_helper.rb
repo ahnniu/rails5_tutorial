512dfe1f88f716175cacccb1ae2a4d45eed45fc7
Yongjia Niu <ahnniu@gmail.com>
2017-07-18 08:30:28 +0800

    1) Add yard, minitest-reporters gems, 2) first test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# Use minitest-reporters to create customizable Minitest output formats
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
