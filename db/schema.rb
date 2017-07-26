1bed8ca8e6937ced393352c66d97b087fa3311dc
Yongjia Niu <ahnniu@gmail.com>
2017-07-26 14:38:07 +0800

    Add secure password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726030907) do
+ActiveRecord::Schema.define(version: 20170726060522) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "123456", password_confirmation: "123456")
   end
 
   test "should be valid" do
@@ -61,6 +62,21 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present(no blank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should not be too short(minimum:6)" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?, "password #{@user.password} is too short, should not be valid"
+  end
+
+  test "password should not be too long(maximum:30)" do
+    @user.password = @user.password_confirmation = "a" * 31
+    assert_not @user.valid?
   end
 
 end
